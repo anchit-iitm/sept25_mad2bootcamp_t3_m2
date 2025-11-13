b332c4d407cf7d7f2dd3c4b5645b1089572cef45
Prokriti Pandit <22f1000807@ds.study.iitm.ac.in>
2025-11-13 12:32:04 +0530

    day4w

---
+++ day4/frontend/src/views/AboutView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <!-- <input v-model="this.info" placeholder="Type something..." /> -->
+     <!-- <button type="button" v-on:click='console.log("yup kuch toh hhya")' @mouseleave="console.log('bye dua m eyad rakhna')"  >Try it</button> -->
+    <h1>{{ this.info }}</h1>
+    <br>
+    <br>
+    <input type="text" v-model="this.State" placeholder="Enter shhhh to get special message" />
+    <br>
+    <br>
+    <button type="button" @click="this.fetchTheMessage(this.State)">Get the message</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      info: null,
+      State: '',
+    };
+  },
+  methods: {
+    fetchTheMessage(state) {
+      if (state === "shhhh") {
+        this.info = "Kya bolti public, welcome to LookBack App!";
+      } else {
+      this.info = "This is the About page of the LookBack App!";
+      }
+    }
+  }
+
+};
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
